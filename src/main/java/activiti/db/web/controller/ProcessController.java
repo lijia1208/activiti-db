44b430911e4184fad2536ac5b169c5c0c868f5e1
maimaivv <815564258@qq.com>
2019-04-17 07:19:23 +0800

    commit fork branch

--- src/main/java/activiti/db/web/controller/ProcessController.java
+++ src/main/java/activiti/db/web/controller/ProcessController.java
@@ -1,9 +1,11 @@
 package activiti.db.web.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author maimaivv
  * @description
  * @date 2019-04-14 06:57
- */
+ */@RestController
 public class ProcessController {
 }
--- src/main/java/activiti/db/web/model/vo/TaskVo.java
+++ src/main/java/activiti/db/web/model/vo/TaskVo.java
@@ -1,9 +1,62 @@
 package activiti.db.web.model.vo;
 
+import java.util.Map;
+
 /**
  * @author maimaivv
  * @description
  * @date 2019-04-16 06:43
  */
 public class TaskVo {
+    private String id;
+    private String name;
+    private String comment;
+    private Map<String, Object> variables;
+
+
+
+
+    public TaskVo(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //TODO effectiveJava 换成Build ??
+    public TaskVo(String id, String name, String comment) {
+        this.id = id;
+        this.name = name;
+        this.comment = comment;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Map<String, Object> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(Map<String, Object> variables) {
+        this.variables = variables;
+    }
 }
